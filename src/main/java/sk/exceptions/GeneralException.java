6e3b050963fbee7136e92374f10115e59ed124f4
Martin <mar.krajc@gmail.com>
2022-01-04 21:26:21 +0100

    added GeneralException and MissingAccount exceptions

---
+++ src/main/java/sk/exceptions/GeneralException.java
@@ -0,0 +1,20 @@
+package sk.exceptions;
+
+import sk.abstract_interface.MessageResolver;
+
+public class GeneralException extends RuntimeException {
+
+	private static final long serialVersionUID = -50378626526532631L;
+
+	private String message;
+	
+	public GeneralException (String message, Object... messageArguments) {
+		super();
+		this.message = MessageResolver.resolveMessage(message, messageArguments);
+	}
+
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+}
