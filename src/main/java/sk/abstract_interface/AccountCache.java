4c6de508680c57e7e6939432a16fc46e59b8e987
Martin <mar.krajc@gmail.com>
2022-01-04 21:20:30 +0100

    added messages cache and account IDs cache

--- src/main/java/sk/abstract_interface/AccountCache.java
+++ src/main/java/sk/abstract_interface/AccountCache.java
@@ -2,11 +2,14 @@ package sk.abstract_interface;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
+import sk.exceptions.MissingAccount;
 
 @Component
 public class AccountCache {
@@ -19,8 +22,37 @@ public class AccountCache {
 		return exchangeRequest.getAllAccounts();
 	}
 
+	@Cacheable(value = CacheNames.ACCOUNT_ID_CACHE, sync = true)
+	public String getAccountIdByCurrency(Currency currency) throws Exception {
+		List<Map<String, Object>> accounts = getAllAccounts();
+
+		Optional<String> accountId = accounts.stream()
+			.filter(account -> hasRequiredCurrency(account, currency))
+			.map(account -> getAccountId(account))
+			.findAny();
+
+		if (accountId.isEmpty()) {
+			throw new MissingAccount("accountNotFound", currency);
+		}
+		
+		return accountId.get();
+	}
+
+	private boolean hasRequiredCurrency(Map<String, Object> account, Currency currency) {
+		return String.valueOf(account.get("currency")).equals(currency.getAcronym());
+	}
+
+	private String getAccountId(Map<String, Object> account) {
+		return String.valueOf(account.get("id"));
+	}
+
 	@CacheEvict(value = CacheNames.ACCOUNTS_CACHE, allEntries = true, beforeInvocation = true)
 	public void dropAccountCache() {
+		// log
+	}
+
+	@CacheEvict(value = CacheNames.ACCOUNT_ID_CACHE, allEntries = true, beforeInvocation = true)
+	public void dropAccountIDCache() {
 		// log
 	}
 }
--- src/main/java/sk/abstract_interface/CacheNames.java
+++ src/main/java/sk/abstract_interface/CacheNames.java
@@ -3,5 +3,6 @@ package sk.abstract_interface;
 public class CacheNames {
 
 	public static final String ACCOUNTS_CACHE = "accounts";
-
+	public static final String ACCOUNT_ID_CACHE = "accountIDs";
+	public static final String MESSAGES_CACHE = "messages";
 }
