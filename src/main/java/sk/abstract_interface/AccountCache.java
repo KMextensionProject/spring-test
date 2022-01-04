bd0a838408e6ecec88394f2658e37da200a51d8c
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-04 15:40:44 +0100

    AccountCache - added dropAccontCache()

--- src/main/java/sk/abstract_interface/AccountCache.java
+++ src/main/java/sk/abstract_interface/AccountCache.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,13 @@ public class AccountCache {
 	@Autowired
 	private ExchangeRequest exchangeRequest;
 
-	// called from multiple locations makes the request call..why?
-	@Cacheable(cacheNames = {"accounts"}, sync = true)
+	@Cacheable(value = CacheNames.ACCOUNTS_CACHE, sync = true)
 	public List<Map<String, Object>> getAllAccounts() throws Exception {
 		return exchangeRequest.getAllAccounts();
 	}
 
+	@CacheEvict(value = CacheNames.ACCOUNTS_CACHE, allEntries = true, beforeInvocation = true)
+	public void dropAccountCache() {
+		// log
+	}
 }
