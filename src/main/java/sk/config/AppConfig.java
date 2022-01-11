9eabebadb2e23fdc4c77a910be30fbd2c0d34e6f
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-11 20:28:08 +0100

    AppConfig - removed HttpClient bean and annotation enabling scheduling tasks

--- src/main/java/sk/config/AppConfig.java
+++ src/main/java/sk/config/AppConfig.java
@@ -2,8 +2,6 @@ package sk.config;
 
 import java.util.Arrays;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -26,15 +23,9 @@ import sk.abstract_interface.Currency;
 
 @Configuration
 @EnableCaching
-@EnableScheduling
 @PropertySource("classpath:account_defaults.properties")
 @ComponentScan(basePackages = "sk")
 public class AppConfig {
-
-	@Bean
-	public HttpClient getHttpClient() {
-		return HttpClientBuilder.create().build();
-	}
 
 	@Bean(name = {"accountCurrency"})
 	public Currency loadDefaultAccountCurrency(@Value("${account.currency}") String currency) {
