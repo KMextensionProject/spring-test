9b000821a3bd99205119d212e833a7ab8ef58e30
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-11 22:21:34 +0100

    CryptoMarket - added proper day adjuster for getting prices from market

--- src/main/java/sk/implementation/CryptoMarket.java
+++ src/main/java/sk/implementation/CryptoMarket.java
@@ -1,9 +1,13 @@
 package sk.implementation;
 
-import static sk.abstract_interface.PriceType.OPENING;
 import static sk.abstract_interface.PriceType.CLOSING;
+import static sk.abstract_interface.PriceType.OPENING;
 
+import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,7 @@ import org.springframework.stereotype.Component;
 import sk.abstract_interface.Market;
 import sk.abstract_interface.PriceType;
 import sk.bl.MarketPredicate;
+import sk.implementation.RequestTime.DateUnit;
 
 @Component
 public class CryptoMarket extends Market {
@@ -22,23 +27,54 @@ public class CryptoMarket extends Market {
 	@Autowired
 	private MarketPredicate marketPredicate;
 
+	@Autowired
+	private RequestTime requestTime;
+
 	@Override
 	public void updateState() throws Exception {
 		this.currentPrice = cryptoMarketRequest.getCurrentPrice();
+		updateOpeningAndClosingPrices();
+	}
 
-		// TODO:
-		// ak je dnes pondelok, tak polygon bude tento request zdrziavat! zvolit nedelu - zatvaraciu cenu?
-//		Map<PriceType, Double> weekPrice = cryptoMarketRequest.getPricesByDate(LocalDate.now().with(DayOfWeek.MONDAY));
-		Map<PriceType, Double> weekPrice = cryptoMarketRequest.getPricesByDate(LocalDate.now().minusDays(1L));
-		this.weekOpeningPrice = weekPrice.get(OPENING);
-		this.weekClosingPrice = weekPrice.get(CLOSING);
+	// TODO: find a better way to express this
+	private void updateOpeningAndClosingPrices() throws IOException {
+		List<DateUnit> registeredDateUnits = marketPredicate.getRegisteredDateUnits();
 
-		Map<PriceType, Double> monthPrice = cryptoMarketRequest.getPricesByDate(LocalDate.now().withDayOfMonth(1));
-		this.monthOpeningPrice = monthPrice.get(OPENING);
-		this.monthClosingPrice = monthPrice.get(CLOSING);
+		if (registeredDateUnits.contains(DateUnit.WEEK)) {
+			Map<PriceType, Double> weekPrice = cryptoMarketRequest.getPricesByDate(getFirstDayAdjusted(DateUnit.WEEK));
+			this.firstDayOfWeekOpeningPrice = weekPrice.get(OPENING);
+			this.firstDayOfWeekClosingPrice = weekPrice.get(CLOSING);
+		}
 
-		Map<PriceType, Double> yearPrice = cryptoMarketRequest.getPricesByDate(LocalDate.now().withDayOfYear(1));
-		this.yearOpeningPrice = yearPrice.get(OPENING);
+		if (registeredDateUnits.contains(DateUnit.MONTH)) {
+			Map<PriceType, Double> monthPrice = cryptoMarketRequest.getPricesByDate(getFirstDayAdjusted(DateUnit.MONTH));
+			this.firstDayOfMonthOpeningPrice = monthPrice.get(OPENING);
+			this.firstDayOfMonthClosingPrice = monthPrice.get(CLOSING);
+		}
+
+		if (registeredDateUnits.contains(DateUnit.YEAR)) {
+			Map<PriceType, Double> yearPrice = cryptoMarketRequest.getPricesByDate(getFirstDayAdjusted(DateUnit.YEAR));
+			this.firstDayOfYearOpeningPrice = yearPrice.get(OPENING);
+			this.firstDayOfYearOpeningPrice = yearPrice.get(CLOSING);
+		}
+	}
+
+	private LocalDate getFirstDayAdjusted(DateUnit dateUnit) {
+		LocalDate date = requestTime.getFirstDayOf(dateUnit);
+		switch (dateUnit) {
+		case WEEK:
+			return (DayOfWeek.MONDAY.equals(date.getDayOfWeek())) ? subtractOneDay(date) : date;
+		case MONTH:
+			return (1 == date.getDayOfMonth()) ? subtractOneDay(date) : date;
+		case YEAR:
+			return (1 == date.getDayOfYear()) ? subtractOneDay(date) : date;
+		default:
+			return date;
+		}
+	}
+
+	private LocalDate subtractOneDay(LocalDate date) {
+		return date.minus(1L, ChronoUnit.DAYS);
 	}
 
 	@Override
