3d6d095c1b05e4121524c28a84a38e9081160d60
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-11 22:18:38 +0100

    MarketPredicate - added toString(), percentage getters and registeredDateUnits field

--- src/main/java/sk/bl/MarketPredicate.java
+++ src/main/java/sk/bl/MarketPredicate.java
@@ -1,11 +1,16 @@
 package sk.bl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import sk.abstract_interface.Market;
+import sk.implementation.RequestTime.DateUnit;
 
 @Component
 public class MarketPredicate {
@@ -19,6 +24,8 @@ public class MarketPredicate {
 	@Value("-${buy_predicate.year:0}")
 	private int year;
 
+	private List<DateUnit> registeredDateUnits;
+
 	public void validateBuyPredicateSetting() {
 		if ((week + month + year) == 0) {
 			throw new IllegalStateException("no predicates specified");
@@ -31,13 +38,13 @@ public class MarketPredicate {
 		Predicate<Market> yearPredicate = m -> false;
 
 		if (week != 0) {
-			weekPredicate = m -> getPercentageDifference(m.getCurrentPrice(), m.getWeekOpeningPrice()) < week;
+			weekPredicate = m -> getPercentageDifference(m.getCurrentPrice(), m.getFirstDayOfWeekOpeningPrice()) < week;
 		}
 		if (month != 0) {
-			monthPredicate = m -> getPercentageDifference(m.getCurrentPrice(), m.getMonthOpeningPrice()) < month;
+			monthPredicate = m -> getPercentageDifference(m.getCurrentPrice(), m.getFirstDayOfMonthOpeningPrice()) < month;
 		}
 		if (year != 0) {
-			yearPredicate = m -> getPercentageDifference(m.getCurrentPrice(), m.getYearOpeningPrice()) < year;
+			yearPredicate = m -> getPercentageDifference(m.getCurrentPrice(), m.getFirstDayOfYearOpeningPrice()) < year;
 		}
 
 		return weekPredicate.or(monthPredicate).or(yearPredicate);
@@ -45,5 +52,39 @@ public class MarketPredicate {
 
 	private int getPercentageDifference(double current, double opening) {
 		return (int) (((current - opening) / opening) * 100);
+	}
+
+	public List<DateUnit> getRegisteredDateUnits() {
+		return this.registeredDateUnits;
+	}
+
+	public int getWeekPredicatePercentage() {
+		return this.week;
+	}
+
+	public int getMonthPredicatePercentage() {
+		return this.month;
+	}
+
+	public int getYearPredicatePercentage() {
+		return this.year;
+	}
+
+	@Override
+	public String toString() {
+		return "week predicate: " + week + "%\n"
+			+ "month predicate: " + month + "%\n"
+			+ "year predicate: " + year + "%\n";
+	}
+
+	@PostConstruct
+	private void initDateUnitList() {
+		registeredDateUnits = new ArrayList<>(3);
+		if (week != 0)
+			registeredDateUnits.add(DateUnit.WEEK);
+		if (month != 0)
+			registeredDateUnits.add(DateUnit.MONTH);
+		if (year != 0)
+			registeredDateUnits.add(DateUnit.YEAR);
 	}
 }
