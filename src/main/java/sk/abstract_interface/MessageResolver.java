33c7d8381fb710d5ae0d986b19f5c0994bc5a641
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-13 08:40:08 +0100

    MessageResolver - logged messages load

--- src/main/java/sk/abstract_interface/MessageResolver.java
+++ src/main/java/sk/abstract_interface/MessageResolver.java
@@ -3,13 +3,18 @@ package sk.abstract_interface;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 public final class MessageResolver {
+	
+	private static final Logger logger = Logger.getLogger(MessageResolver.class);
 
 	private static Properties properties = new Properties();
 
 	static {
 		try {
 			properties.load(MessageResolver.class.getClassLoader().getResourceAsStream(Resources.MESSAGES_MAP));
+			logger.info(resolveMessage("messageResolver"));
 		} catch (IOException readingFailure) {
 			readingFailure.printStackTrace();
 		}
