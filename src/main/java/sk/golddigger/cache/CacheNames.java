ec7b6293c5774fe3fb1b4ed128c8bee883720e07
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-13 20:26:09 +0100

    CacheNames - removed unused cache name

--- src/main/java/sk/golddigger/cache/CacheNames.java
+++ src/main/java/sk/golddigger/cache/CacheNames.java
@@ -4,5 +4,4 @@ public class CacheNames {
 
 	public static final String ACCOUNTS_CACHE = "accounts";
 	public static final String ACCOUNT_ID_CACHE = "accountIDs";
-	public static final String MESSAGES_CACHE = "messages";
 }
