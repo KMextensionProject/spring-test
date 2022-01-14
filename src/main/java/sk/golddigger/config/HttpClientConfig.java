3a593e4f7e738d8ce57f53b548b6a2d316a787cf
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-14 22:25:41 +0100

    HttpClientConfig - organized imports

--- src/main/java/sk/golddigger/config/HttpClientConfig.java
+++ src/main/java/sk/golddigger/config/HttpClientConfig.java
@@ -10,7 +10,6 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * This configuration class provides the HttpClient bean for executing HTTP
