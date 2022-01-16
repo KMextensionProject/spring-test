6c32a1b2f76f83a78e154abe5449fc159b5eb3d1
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-16 17:26:16 +0100

    MarketPredicate - added explanatory comment

--- src/main/java/sk/golddigger/core/MarketPredicate.java
+++ src/main/java/sk/golddigger/core/MarketPredicate.java
@@ -5,6 +5,13 @@ import java.util.function.Predicate;
 
 import sk.golddigger.core.RequestDateTime.DateUnit;
 
+/**
+ * This class represents a predicate that in its principle compares the 
+ * week, month and year percentage for market decrease specified by the
+ * initial configuration.</br>
+ * It also allows to add additional market conditions to the underlying 
+ * predicate.
+ */
 public abstract class MarketPredicate {
 
 	protected int week;
