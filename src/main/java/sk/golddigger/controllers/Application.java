6f9e368a9f49eaf2df8dbbf295df3e35a869373d
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-18 21:42:33 +0100

    Application.ping() - added @ResponseBody annotation

--- src/main/java/sk/golddigger/controllers/Application.java
+++ src/main/java/sk/golddigger/controllers/Application.java
@@ -2,11 +2,13 @@ package sk.golddigger.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Application {
 
 	@GetMapping(path = "/ping", produces = "text/plain")
+	@ResponseBody
 	public String ping() {
 		return "Status OK";
 	}
