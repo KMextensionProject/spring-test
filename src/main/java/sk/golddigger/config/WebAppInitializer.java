cafd0e097ca19b40eb1e1a7e1db98438423107b9
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-19 22:04:51 +0100

    WebAppInitializer - added ahead ..viewResolvers() and ..interceptors()

--- src/main/java/sk/golddigger/config/WebAppInitializer.java
+++ src/main/java/sk/golddigger/config/WebAppInitializer.java
@@ -1,9 +1,20 @@
 package sk.golddigger.config;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class WebAppInitializer implements WebMvcConfigurer {
 
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// add jsp with prefix
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// add logging interceptor
+	}
 }
