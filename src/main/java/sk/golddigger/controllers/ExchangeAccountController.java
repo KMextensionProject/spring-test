68e831c02c6c8299db13f19878caf6c7bbc36c5e
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-20 16:30:31 +0100

    ExchangeAccountService/Controller - added getAccountComplexOverview() service

--- src/main/java/sk/golddigger/controllers/ExchangeAccountController.java
+++ src/main/java/sk/golddigger/controllers/ExchangeAccountController.java
@@ -1,8 +1,24 @@
 package sk.golddigger.controllers;
 
+import static sk.golddigger.http.ContentType.APPLICATION_JSON;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import sk.golddigger.services.ExchangeAccountService;
 
 @Controller
 public class ExchangeAccountController {
+
+	@Autowired
+	private ExchangeAccountService exchangeService;
+
+	@GetMapping(path = "/account/complexOverview", produces = APPLICATION_JSON)
+	public Map<String, Object> getAccountComplexOverview() {
+		return exchangeService.getAccountComplexOverview();
+	}
 
 }
--- src/main/java/sk/golddigger/services/ExchangeAccountService.java
+++ src/main/java/sk/golddigger/services/ExchangeAccountService.java
@@ -1,8 +1,61 @@
 package sk.golddigger.services;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import sk.golddigger.cache.AccountCache;
+import sk.golddigger.core.ExchangeAccount;
+import sk.golddigger.core.ExchangeRequest;
+import sk.golddigger.enums.Currency;
 
 @Service
 public class ExchangeAccountService {
 
+	@Autowired
+	private ExchangeAccount exchangeAccount;
+
+	@Autowired
+	private ExchangeRequest exchangeRequest;
+
+	@Autowired
+	private AccountCache accountCache;
+
+	public Map<String, Object> getAccountComplexOverview() {
+	    Map<String, Object> accountComplexOverview = new LinkedHashMap<>(2);
+	    accountComplexOverview.put("default_account", getDefaultAccountInfo());
+	    accountComplexOverview.put("trading_account", getTradingAccountInfo());
+
+	    return accountComplexOverview;
+	  }
+
+	  private Map<String, Object> getDefaultAccountInfo() {
+	    Currency accountCurrency = exchangeAccount.getAccountCurrency();
+
+	    Map<String, Object> defaultAccountInfo = new LinkedHashMap<>();
+	    defaultAccountInfo.put("account_id", exchangeAccount.getAccountId());
+	    defaultAccountInfo.put("currency", accountCurrency.getName());
+	    defaultAccountInfo.put("currency_acronym", accountCurrency.getAcronym());
+	    defaultAccountInfo.put("balance", exchangeAccount.getBalance());
+	    defaultAccountInfo.put("best_buy_rate", exchangeAccount.getBestOrderBuyRate());
+
+	    return defaultAccountInfo;
+	  }
+
+	  private Map<String, Object> getTradingAccountInfo() {
+	    Currency tradingCurrency = exchangeAccount.getTradingCurrency();
+
+	    String tradingAccountId = accountCache.getAccountIdByCurrency(tradingCurrency);
+	    double tradingAccountBalance = exchangeRequest.getAccountBalance(tradingAccountId);
+
+	    Map<String, Object> tradingAccountInfo = new LinkedHashMap<>();
+	    tradingAccountInfo.put("account_id", tradingAccountId);
+	    tradingAccountInfo.put("currency", tradingCurrency.getName());
+	    tradingAccountInfo.put("currency_acronym", tradingCurrency.getAcronym());
+	    tradingAccountInfo.put("balance", tradingAccountBalance);
+
+	    return tradingAccountInfo;
+	  }
 }
