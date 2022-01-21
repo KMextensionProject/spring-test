3b2803afcc16fc2817a8f28220b6814333361d59
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-21 15:51:20 +0100

    added XlsxUtils class responsible for generating xlsx documents

---
+++ src/main/java/sk/golddigger/utils/XlsxUtils.java
@@ -0,0 +1,19 @@
+package sk.golddigger.utils;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ */
+// TODO: implement
+public class XlsxUtils {
+
+	public static void generateXlsx(HttpServletResponse response) {
+		
+	}
+
+	private static void addResponseHeaders() {
+		
+	}
+
+}
