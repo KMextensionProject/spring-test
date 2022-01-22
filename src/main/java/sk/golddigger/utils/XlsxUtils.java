099a3163bc2fcff5db4281ef8576cc1e1fda7809
Martin Krajcovic <mar.krajc@gmail.com>
2022-01-22 16:14:42 +0100

    XlsxUtils - added addHeaders()

--- src/main/java/sk/golddigger/utils/XlsxUtils.java
+++ src/main/java/sk/golddigger/utils/XlsxUtils.java
@@ -1,11 +1,11 @@
 package sk.golddigger.utils;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.Base64;
 import java.util.Map;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -16,24 +16,24 @@ import net.sf.jett.transform.ExcelTransformer;
 /**
  * 
  */
-// TODO: implement
 public class XlsxUtils {
 
-	public static void generateXlsx(String template, Map<String, Object> dataSource, String targetName, HttpServletResponse response) throws InvalidFormatException, FileNotFoundException, IOException {
-		InputStream input = XlsxUtils.class.getClassLoader().getResourceAsStream(template);
-		
+	public static void generateXlsx(String template, Map<String, Object> dataSource, String targetName, HttpServletResponse response) throws IOException, InvalidFormatException {
+		InputStream inputStream = XlsxUtils.class.getClassLoader().getResourceAsStream(template);
+
 		addResponseHeaders(response, targetName);
-		OutputStream output = response.getOutputStream();
-		
+		ServletOutputStream outputStream = response.getOutputStream();
+
 		ExcelTransformer transformer = new ExcelTransformer();
-		Workbook workbook = transformer.transform(input, dataSource);
-		workbook.write(output);
-		
+		Workbook workbook = transformer.transform(inputStream, dataSource);
+		workbook.write(outputStream);
 	}
 
 	private static void addResponseHeaders(HttpServletResponse response, String targetName) {
-		
-		
+		response.addHeader("Content-type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+		String encoded = Base64.getEncoder().encodeToString(targetName.getBytes());
+		String contentDisposition = "attachment; filename=\"=?UTF-8?B?" + encoded + "?=\"";
+		response.addHeader("Content-disposition", contentDisposition);
 	}
 
 }
