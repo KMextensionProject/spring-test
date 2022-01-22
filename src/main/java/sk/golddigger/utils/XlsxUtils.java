6373e76500895fb157c1d77f5a748add02054bf1
Miska Lehotska <misela.le@gmail.com>
2022-01-22 16:01:31 +0100

    Added generate xlsx method

--- src/main/java/sk/golddigger/utils/XlsxUtils.java
+++ src/main/java/sk/golddigger/utils/XlsxUtils.java
@@ -1,6 +1,17 @@
 package sk.golddigger.utils;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import net.sf.jett.transform.ExcelTransformer;
 
 /**
  * 
@@ -8,11 +19,20 @@ import javax.servlet.http.HttpServletResponse;
 // TODO: implement
 public class XlsxUtils {
 
-	public static void generateXlsx(HttpServletResponse response) {
+	public static void generateXlsx(String template, Map<String, Object> dataSource, String targetName, HttpServletResponse response) throws InvalidFormatException, FileNotFoundException, IOException {
+		InputStream input = XlsxUtils.class.getClassLoader().getResourceAsStream(template);
+		
+		addResponseHeaders(response, targetName);
+		OutputStream output = response.getOutputStream();
+		
+		ExcelTransformer transformer = new ExcelTransformer();
+		Workbook workbook = transformer.transform(input, dataSource);
+		workbook.write(output);
 		
 	}
 
-	private static void addResponseHeaders() {
+	private static void addResponseHeaders(HttpServletResponse response, String targetName) {
+		
 		
 	}
 
