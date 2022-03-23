1cca804833504e71c8f529303ee59f49b913f79f
Martin Krajcovic <mar.krajc@gmail.com>
2022-03-23 18:29:25 +0100

    created SMSProvider empty class

---
+++ src/main/java/sk/golddigger/messaging/SMSProvider.java
@@ -0,0 +1,10 @@
+package sk.golddigger.messaging;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Lazy
+@Component
+public class SMSProvider {
+	
+}
