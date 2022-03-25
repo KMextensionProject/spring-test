26bbac4f78b566daacb93c4e0ef6afd621cb0ed6
Martin Krajcovic <mar.krajc@gmail.com>
2022-03-25 05:58:03 +0100

    AppConfig - added comment on Gson FieldNamingPolicy setting

--- src/main/java/sk/golddigger/config/AppConfig.java
+++ src/main/java/sk/golddigger/config/AppConfig.java
@@ -78,6 +78,7 @@ public class AppConfig {
 	@Bean
 	public Gson getPrettyPrintingGson() {
 		return new GsonBuilder()
+				// because the Coinbase exchange API uses this style (Binance uses cammelCase)
 				.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
 				.setPrettyPrinting()
 				.create();
