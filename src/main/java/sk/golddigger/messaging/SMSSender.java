23aafebc0358d82ae7ccf21d3cd74d1010cecc16
Martin Krajcovic <mar.krajc@gmail.com>
2022-03-26 17:46:19 +0100

    added SMS interface hierarchy

---
+++ src/main/java/sk/golddigger/messaging/SMSSender.java
@@ -0,0 +1,11 @@
+package sk.golddigger.messaging;
+
+public abstract class SMSSender {
+
+	/**
+	 * Safely send SMS message.
+	 * @return true if the message was sent successfully, false otherwise;
+	 */
+	public abstract boolean send(Message message, Recipient recipient);
+
+}
--- src/main/java/sk/golddigger/notification/SMSNotification.java
+++ src/main/java/sk/golddigger/notification/SMSNotification.java
@@ -5,12 +5,14 @@ import static sk.golddigger.utils.MessageResolver.resolveMessage;
 import javax.annotation.PostConstruct;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import sk.golddigger.annotations.OnPropertyContent;
 import sk.golddigger.enums.RegexPatterns;
 import sk.golddigger.messaging.Message;
 import sk.golddigger.messaging.Recipient;
+import sk.golddigger.messaging.SMSSender;
 
 @OnPropertyContent(propertyName = "NOTIFICATION_RECIPIENT", lookupRegex = RegexPatterns.PHONE)
 @Component
@@ -18,9 +20,19 @@ public class SMSNotification implements Notification {
 
 	private static final Logger logger = Logger.getLogger(SMSNotification.class);
 
+	@Autowired
+	private SMSSender smsSender;
+
 	@Override
 	public void send(Message message, Recipient recipient) {
-		// TODO: implement Twillio provider
+		boolean notificationSent = smsSender.send(message, recipient);
+
+		// TODO: add this to messages.properties
+		if (notificationSent) {
+			logger.info(resolveMessage("smsNotificationSuccessful", recipient.getPhoneNumber()));
+		} else {
+			logger.warn(resolveMessage("smsNotificationUnsuccessful", recipient.getPhoneNumber()));
+		}
 	}
 
 	@PostConstruct
