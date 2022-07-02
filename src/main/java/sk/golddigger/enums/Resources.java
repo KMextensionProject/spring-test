c038fb49536e2bbd1e2b3f85a6b3f10125d64241
Martin Krajcovic <mar.krajc@gmail.com>
2022-07-02 11:18:25 +0200

    [INFRA] altered default Jackson Http2MessageConverter to use indentation on output data

--- src/main/java/sk/golddigger/enums/Resources.java
+++ src/main/java/sk/golddigger/enums/Resources.java
@@ -7,6 +7,8 @@ import static sk.golddigger.utils.MessageResolver.resolveMessage;
  */
 public class Resources {
 
+	public static final String JSP_VIEW_RESOURCES = "/WEB-INF/views/";
+
 	public static final String CURRENT_CRYPTO_PRICE_URL = "https://api.coingecko.com/api/v3/coins/$?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false";
 
 	public static final String CRYPTO_PRICE_BY_DATE_URL = "https://api.polygon.io/v2/aggs/ticker/X:$$/range/1/day/$/$?adjusted=true&sort=asc&limit=120&apiKey=$";
