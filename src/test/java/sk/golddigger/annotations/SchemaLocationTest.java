d484ea527609adadf70a81de145113a3e7450bc9
Martin Krajcovic <mar.krajc@gmail.com>
2022-07-05 17:30:12 +0200

    added test folder for unit tests with TypeUtils tests

---
+++ src/test/java/sk/golddigger/annotations/SchemaLocationTest.java
@@ -0,0 +1,16 @@
+package sk.golddigger.annotations;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class SchemaLocationTest {
+
+	@Test
+	@Disabled
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/sk/golddigger/utils/MessageResolverTest.java
@@ -0,0 +1,16 @@
+package sk.golddigger.utils;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class MessageResolverTest {
+
+	@Test
+	@Disabled
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
