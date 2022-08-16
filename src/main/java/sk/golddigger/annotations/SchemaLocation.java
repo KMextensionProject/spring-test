55e114ba4a39e3f86194c16d371b22880a0c65bd
Martin Krajcovic <mar.krajc@gmail.com>
2022-08-16 18:25:30 +0200

    added SchemaLocation annotation to be put on controllers

---
+++ src/main/java/sk/golddigger/annotations/SchemaLocation.java
@@ -0,0 +1,18 @@
+package sk.golddigger.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface SchemaLocation {
+
+	String inputPath() default "";
+
+	String outputPath() default "";
+
+	boolean noSchema() default false;
+
+}
